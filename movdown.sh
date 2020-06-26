2b771fd81b23d79076f5bf7233ca522ad33e4b8c
sci anto <65482441+Scianto@users.noreply.github.com>
2020-06-26 14:49:22 +0530

    Add files via upload

---
+++ movdown.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#Movdown v 1.1
+#this tool is mainly for downloading movies
+#author : sci_anto
+#github : https://github.com/Scianto/Movdown 
+
+banner () {
+
+printf "                               " 
+printf "                               "
+printf "                               "
+printf "                               "
+printf "                               "
+printf "    Created by Sci_anto"
+
+printf" Disclaimer : IN this tool you can download all languagae movies :: ITS ONLY FOR EDUCATION PURPOSE "
+
+scanner() {
+
+echo -n $'\e[1;92m Enter the movie name:\e[0m ' movie
+sum=`expr https://tamilrockers.unblockit.id/index.php  + $movie`
+
+echo "your movie url: $sum"
+
+echo "happy watching !!!!!!"
+
+}
+banner
+scanner
+
+	
